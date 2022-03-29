f8b0c86687ac61cafd6469b7e88165f64c3c8fdf
MrDydymaxx <mrdydymmaxx@gmail.com>
2022-03-29 23:28:24 +0200

    add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import homeReducer from './home';
+import authReducer from './auth';
+import statsReducer from './stats';
+import searchReducer from './search';
+
+const reducers = {
+  home: homeReducer,
+  auth: authReducer,
+  stats: statsReducer,
+  search: searchReducer
+};
+
+export default reducers;
